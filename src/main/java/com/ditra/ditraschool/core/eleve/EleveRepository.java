08661fd74d2c50f93cd1f690390eef5b67b0077b
Wael Ben Taleb <waelben7@gmail.com>
2019-10-03 14:14:00 +0100

    ok bb

--- src/main/java/com/ditra/ditraschool/core/eleve/EleveRepository.java
+++ src/main/java/com/ditra/ditraschool/core/eleve/EleveRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EleveRepository extends JpaRepository<Eleve, Long> {
-  public List<Eleve> findAllByInscriptionsEmpty();
+  List<Eleve> findAllByInscriptionsEmpty();
 }
