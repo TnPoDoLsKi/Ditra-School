709cc2ccd7f1027450bdd416fbe39c7ab09d7c6b
Wael Ben Taleb <waelben7@gmail.com>
2019-10-04 10:23:34 +0100

    mass mta3 session

--- src/main/java/com/ditra/ditraschool/config/SecureURLs.java
+++ src/main/java/com/ditra/ditraschool/config/SecureURLs.java
@@ -7,7 +7,6 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 public class SecureURLs {
     private static final RequestMatcher PROTECTED_URLS = new OrRequestMatcher(
 
-        //UserController
         new AntPathRequestMatcher("/api/v1/logout"),
         new AntPathRequestMatcher("/api/v1/verifyAccess"),
         new AntPathRequestMatcher("/api/v1/article/**"),
@@ -17,7 +16,7 @@ public class SecureURLs {
         new AntPathRequestMatcher("/api/v1/global/**"),
         new AntPathRequestMatcher("/api/v1/inscription/**"),
         new AntPathRequestMatcher("/api/v1/paiement/**")
-        );
+    );
 
     public static RequestMatcher getProtectedUrls() {
         return PROTECTED_URLS;
