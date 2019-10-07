e8f43781cf150118662b650f6c0eea506af335cb
ousema <masmoudiousema9@gmail.com>
2019-10-07 16:14:54 +0100

    Article beaucoup

--- src/main/java/com/ditra/ditraschool/core/paiement/PaiementController.java
+++ src/main/java/com/ditra/ditraschool/core/paiement/PaiementController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/api/v1")
 public class PaiementController {
 
 
