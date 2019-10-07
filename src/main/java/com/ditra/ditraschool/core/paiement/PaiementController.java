1dd4f9d0c3af40650a6586ed1ebb2dfad72bc418
Wael Ben Taleb <waelben7@gmail.com>
2019-10-07 11:03:05 +0100

    mass ya ghoul

--- src/main/java/com/ditra/ditraschool/core/paiement/PaiementController.java
+++ src/main/java/com/ditra/ditraschool/core/paiement/PaiementController.java
@@ -2,6 +2,7 @@ package com.ditra.ditraschool.core.paiement;
 
 
 import com.ditra.ditraschool.core.paiement.models.PaiementModel;
+import com.ditra.ditraschool.core.paiement.models.PaiementUpdate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,7 +26,7 @@ public class PaiementController {
   public ResponseEntity<?> create(@RequestBody PaiementModel paiementModel) { return paiementServices.create(paiementModel); }
 
   @PutMapping("/paiement/{id}")
-  public ResponseEntity<?> update(@PathVariable Long id , @RequestBody PaiementModel paiementModel) { return paiementServices.update(id , paiementModel); }
+  public ResponseEntity<?> update(@PathVariable Long id , @RequestBody PaiementUpdate paiementUpdate) { return paiementServices.update(id , paiementUpdate); }
 
   @DeleteMapping("/paiement/{id}")
   public ResponseEntity<?> delete(@PathVariable Long id) { return paiementServices.delete(id); }
